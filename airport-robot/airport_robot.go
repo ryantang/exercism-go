0212ec3f911720717c25ab8f3ab86d42bc696358
Ryan Tang <ryantang@gmail.com>
2025-10-26 07:39:19 -0700

    Finished airport-robot exercise

---
+++ airport-robot/airport_robot.go
@@ -0,0 +1,31 @@
+package airportrobot
+
+type Greeter interface {
+	LanguageName() string
+	Greet(name string) string
+}
+
+func SayHello(name string, g Greeter) string {
+	return "I can speak " + g.LanguageName() + ": " + g.Greet(name)
+	// return fmt.Sprintf("I can speak %s: %s", g.LanguageName(), g.Greet(name))
+}
+
+type Italian struct{}
+
+func (i Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (i Italian) Greet(name string) string {
+	return "Ciao " + name + "!"
+}
+
+type Portuguese struct{}
+
+func (p Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (p Portuguese) Greet(name string) string {
+	return "Olá " + name + "!"
+}
